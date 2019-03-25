7306ab26de0bf1f944b8ac61eac554cfa7a70bc3
Vanshika Gulati <vanshikag345@gmail.com>
2019-03-25 11:05:03 +0000

    Added bootstrap to form

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   get 'testimonials', to: 'pages#testimonials'
+  resources :contacts, only: :create
+  get 'get-in-touch', to: 'contacts#new', as: 'new_contact'
 end
